8ff85eb8bb5a920feb5974c5147529371c432205
Jessica <jehong@ethz.ch>
2025-04-28 17:06:15 +0200

    further simplified args

--- activeuf/create_prompts_dataset.py
+++ activeuf/create_prompts_dataset.py
@@ -6,6 +6,8 @@ from datasets import load_dataset
 from activeuf.schemas import Prompt
 from activeuf.utils import get_logger
 
+logger = get_logger(__name__)
+
 """
 This script downloads a dataset from HuggingFace and processes it into a dataset of prompts (that follows the Prompt schema in `schemas.py`).
 
@@ -13,6 +15,7 @@ Example run command from project root:
     python -m activeuf.create_prompts_dataset \
         --dataset_path allenai/ultrafeedback_binarized_cleaned \
         --dataset_split train_prefs
+        --output_path 
 """
 
 def parse_args() -> argparse.Namespace:
@@ -22,9 +25,17 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--dataset_path", type=str, help="The HuggingFace path of the dataset to extract prompts from (e.g. allenai/ultrafeedback_binarized_cleaned)")
     parser.add_argument("--dataset_split", type=str, help="The split of the dataset to use (e.g. train_prefs, test_prefs)")
 
-    parser.add_argument("--datasets_dir", type=str, default="datasets/", help="The parent directory for datasets")
+    parser.add_argument("--output_path", type=str, help="Where to save the prompts dataset")
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if args.output_path is None:
+        args.output_path = os.path.join("datasets", args.dataset_path)
+        if args.dataset_split:
+            args.output_path = os.path.join(args.output_path, args.dataset_split)
+        logger.info(f"Exporting to {args.output_path} because no output path was provided")
+
+    return args
 
 def construct_prompt_from_sample(sample: dict) -> dict:
     """
@@ -33,15 +44,8 @@ def construct_prompt_from_sample(sample: dict) -> dict:
     """
     return Prompt(**sample).model_dump()
 
-logger = get_logger(__name__)
 if __name__ == "__main__":
     args = parse_args()
-    
-    # Prepare output path
-    output_path = os.path.join(
-        args.datasets_dir, args.dataset_path, args.dataset_split
-    )
-    assert not os.path.exists(output_path)
 
     # Load HF data
     logger.info(f"Loading dataset from {args.dataset_path} (split={args.dataset_split})")
@@ -52,5 +56,5 @@ if __name__ == "__main__":
     prompts = dataset.map(construct_prompt_from_sample, remove_columns=dataset.column_names)
 
     # Export prompts dataset
-    logger.info(f"Saving prompts to {output_path}")
-    prompts.save_to_disk(output_path)
+    logger.info(f"Saving prompts to {args.output_path}")
+    prompts.save_to_disk(args.output_path)
