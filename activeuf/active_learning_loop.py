564963f898141277f184f9b32c0a0361931a65b6
Marian Schneider <smarian@ethz.ch>
2025-05-07 13:56:48 +0200

    Refactored `active_learning_loop.py`

--- activeuf/active_learning_loop.py
+++ activeuf/active_learning_loop.py
@@ -1,129 +1,132 @@
-import random
-import torch
 import json
 import argparse
 import yaml
+import os
 import numpy as np
+from tqdm import tqdm
+
 from torch.utils.data import DataLoader
-from activeuf.uncertainty_quantification.classes import UQTokenizer, UQModelClass, UQTrainer
+from datasets import load_from_disk, Dataset
+
+from rewarduq.models import ENNRewardModelPipeline
+
 from activeuf.oracle.oracles import BaseOracle, RandomOracle, UltraFeedbackOracle
 from activeuf.acquisition_function.acquisition import RandomAcquisitionFunction, DoubleThompsonSampling
+from activeuf.utils import get_logger, setup, set_seed
+from activeuf.configs import *
+from activeuf.schemas import *
 
-def load_prompts_with_completions(completion_dataset, batch_size, shuffle=True) -> DataLoader:
-    data = []
+logger = get_logger(__name__)
 
-    with open(completion_dataset, 'r') as f:
-        first_char = f.read(1)
-        f.seek(0)  # Reset file pointer
+def parse_args() -> argparse.Namespace:
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--uq_model_path", type=str, help="Path to uncertainty quantification model.")
+    parser.add_argument("--uq_model_config", type=str, help="Path to uncertainty quantification config.")
+    parser.add_argument("--uq_trainer_path", type=str, help="Path to uncertainty quantification trainer.")
 
-        if first_char == '[':
-            # Standard JSON array
-            data = json.load(f)
-        else:
-            # Assume JSON Lines format
-            for line in f:
-                if line.strip():  # skip empty lines
-                    data.append(json.loads(line))
+    parser.add_argument("--completion_dataset", type=str, required=True, help="Path to the prompt dataset.")
+    parser.add_argument("--output_size", type=int, default=None, help="Desired output size of the dataset. If not provided, the entire input dataset will be used")
+    parser.add_argument("--output_path", type=str, help="Path to save the annotated dataset.")
 
-    data = np.array(data)    
-    collate_fn = lambda batch: {key: [d[key] for d in batch] for key in batch[0].keys()}
-
-    dataloader = DataLoader(data, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+    parser.add_argument("--num_iterations", type=int, default=10, help="Number of iterations in uncertainty sampling.")
+    parser.add_argument("--batch_size", type=int, default=3, help="Batch Size for uncertainty sampling.")
     
-    return dataloader
+    parser.add_argument("--acquisition_function_type", type=str, default="double_thompson_sampling", help="Acquistion function type")
+    parser.add_argument("--acquisition_config", type=str, default="activeuf/acquisition_function/acquisition_config.yaml", help="acquisition function configuration file path")
+    args = parser.parse_args()
 
+    if not args.output_path:
+        args.output_path = f"{args.dataset_path.rstrip('/')}-active"
+    assert not os.path.exists(args.output_path), f"Output path {args.output_path} already exists"
 
-def prompts_with_two_completions(prompts_with_completions, rewards, uncertainty, acquisition_function):
-    # An array consisting of 2-element arrays (chosen completion indices)
-    selected_ids_per_prompt = acquisition_function(rewards, uncertainty)
-    prompts_with_two_completions_for_annotation = []
+    return args
 
-    for idx, selected_ids in enumerate(selected_ids_per_prompt):
-        prompt = prompts_with_completions["instruction"][idx]
-        completions = prompts_with_completions['completions'][idx]
-        
-        selected_completion1 = completions[selected_ids[0]]
-        selected_completion2 = completions[selected_ids[1]]
+if __name__ == "__main__":
+    args = parse_args()
 
-        prompts_with_two_completions_for_annotation.append({
-            "prompt": prompt,
-            "selected1": selected_completion1,
-            "selected2": selected_completion2,
-        })
+    logger.info("Logging into HuggingFace")
+    setup(login_to_hf=True)
 
-    return prompts_with_two_completions_for_annotation
+    if args.seed:
+        logger.info(f"Setting random seed to {args.seed}")
+        set_seed(args.seed)
 
+    logger.info("Parsing config")
+    try:
+        # Attempt to load the reward configuration file
+        with open(args.acquisition_config, "r") as f:
+            acquisition_config = yaml.safe_load(f)
+    except FileNotFoundError:
+        print(f"Error: The specified reward configuration file '{args.acquisition_config}' was not found.")
+    except yaml.YAMLError as e:
+        print(f"Error: Failed to parse the reward configuration file '{args.reward_config}'.")
+        print(f"Details: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred while loading the reward configuration file: {e}")
 
-def save_ultrafeedback_format(prompts_with_completions_for_annotation, output_path):
+    logger.info(f"Loading Input Dataset {args.dataset_path}")
+    dataset = load_from_disk(args.dataset_path)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
-    # Save to .jsonl
-    with open(output_path, 'w') as f:
-        for prompt in prompts_with_completions_for_annotation:
-            f.write(json.dumps(prompt) + '\n')
-
-    print(f"✅ Saved {len(prompts_with_completions_for_annotation)} records to {output_path}")
-    return prompts_with_completions_for_annotation    
-
-
-def uncertainty_sampling_loop(uq_model_path, uq_model_config, uq_trainer_path, completion_dataset, num_iterations, acquisition_function_type, batch_size, acquisition_config):
-    uq_tokenizer = UQTokenizer.from_pretrained(uq_model_path, uq_model_config)
-    uq_model = UQModelClass.from_pretrained(uq_model_path)
-    uq_trainer = UQTrainer(UQTokenizer, uq_model, uq_trainer_path)
-
-    dataset = []
-    oracle = Oracle()
-    
-    if acquisition_function_type == "double_thompson_sampling":
+    logger.info(f"Creating acquisition function {args.acquisition_function_type}")
+    if args.acquisition_function_type == "double_thompson_sampling":
         max_iterations = acquisition_config.get("max_iterations", 10)
         beta = acquisition_config.get("beta", 1)
         acquisition_function = DoubleThompsonSampling(beta=beta, max_iterations=max_iterations) # will be changed later.
-    else: 
+    elif args.acquisition_function_type == "random":
         acquisition_function = RandomAcquisitionFunction()
+    else:
+        raise ValueError(f"Unknown acquisition function type: {args.acquisition_function_type}")
 
-    prompts_with_completions_dataloader = load_prompts_with_completions(completion_dataset, batch_size)
-    
-    iteration_number = 0
-    for prompts_with_completions in prompts_with_completions_dataloader:
-        inputs = uq_tokenizer(prompts_with_completions)  # Dict[str, torch.Tensor] - Dictionary with keys “input_ids” and “attention_maks”
-        rewards, uncertainty = uq_trainer.model(inputs)  # torch.Tensors with Shape: (n_prompts, n_completions)
-        prompts_with_completions_for_annotation = prompts_with_two_completions(prompts_with_completions, rewards, uncertainty, acquisition_function)
-        labels = oracle(prompts_with_completions_for_annotation)
-        dataset.append((prompts_with_completions_for_annotation, labels))
-        uq_trainer.training_step(prompts_with_completions_for_annotation, labels)
-        if iteration_number == num_iterations - 1: break 
-        else: iteration_number += 1
-        
+    logger.info(f"Creating oracle {args.oracle_type}")
+    if args.oracle_type == "random":
+        oracle = RandomOracle()
+    elif args.oracle_type == "ultrafeedback":
+        oracle = UltraFeedbackOracle()
+    else:
+        raise ValueError(f"Unknown oracle type: {args.oracle_type}")
 
-    #TODO: save dataset, Martin's job. (We can add prompt_ids here)
-    
-def main(config):
-    try:
-        # Attempt to load the reward configuration file
-        with open(config.acquisition_config, "r") as f:
-            acquisition_config = yaml.safe_load(f)
-    except FileNotFoundError:
-        print(f"Error: The specified reward configuration file '{config.acquisition_config}' was not found.")
-        print("Continuing with default parameters")
-    except yaml.YAMLError as e:
-        print(f"Error: Failed to parse the reward configuration file '{config.reward_config}'.")
-        print(f"Details: {e}")
-        print("Continuing with default parameters")
-    except Exception as e:
-        print(f"An unexpected error occurred while loading the reward configuration file: {e}")
-        print("Continuing with default parameters")
+    logger.info(f"Creating UQ model {args.uq_model_path}")
+    if args.acquisition_function_type != "random":
+        # * If the acquisition function is random, there is no need to calculate uncertainties
+        # TODO: Pass correct arguments to the UQ model
+        uq_pipeline = ENNRewardModelPipeline()
 
-    uncertainty_sampling_loop(config.uq_model_path, config.uq_model_config, config.uq_trainer_path, config.completion_dataset, config.num_iterations, config.acquisition_function_type, config.batch_size, acquisition_config)
+    logger.info(f"Starting data generation loop")
+    output_dataset = Dataset.from_dict({
+        "prompt": [],
+        "chosen": [],
+        "rejected": []
+    })
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train reward model using reward config YAML.")
-    parser.add_argument("--uq-model-path", type=str, help="Path to uncertainty quantification model.")
-    parser.add_argument("--uq-model-config", type=str, help="Path to uncertainty quantification config.")
-    parser.add_argument("--uq-trainer-path", type=str, help="Path to uncertainty quantification trainer.")
-    parser.add_argument("--completion-dataset", type=str, required=True, help="Path to the prompt dataset.")
-    parser.add_argument("--num-iterations", type=int, default=10, help="Number of iterations in uncertainty sampling.")
-    parser.add_argument("--batch-size", type=int, default=3, help="Batch Size for uncertainty sampling.")
-    parser.add_argument("--acquisition_function_type", type=str, default="double_thompson_sampling", help="Acquistion function type")
-    parser.add_argument("--acquisition_config", type=str, default="activeuf/acquisition_function/acquisition_config.yaml", help="acquisition function configuration file path")
-    config = parser.parse_args()
+    for batch in tqdm(dataloader, total=args.output_size // args.batch_size):
+        # Get reward and uncertainty (lower and upper bounds)
+        result = uq_pipeline(batch)
+        reward, lower_bound, upper_bound = result[0, :], result[1, :], result[2, :]
 
-    main(config)
+        # Select the completions that should be used for the binarized sample
+        selected_idx = acquisition_function(reward, lower_bound, upper_bound)
+        selected_completions = None  # TODO
+
+        # Call oracle to determine which is chosen and which is rejected
+        labels = oracle(selected_completions)
+
+        # Add the batch to the output dataset
+        # TODO: Check if this is done properly
+        output_dataset = output_dataset.add_item({
+            "prompt": batch["prompt"],
+            "chosen": None,
+            "rejected": None
+        })
+
+        # Train the reward model with the new data
+        uq_pipeline.train(output_dataset, labels)
+
+    logger.info(f"Saving output dataset to {args.output_path}")
+    os.makedirs(args.output_path, exist_ok=True)
+    output_dataset.save_to_disk(args.output_path)
+
+    args_path = os.path.join(args.output_path, "args.json")
+    with open(args_path, "w") as f_out:
+        json.dump(vars(args), f_out)
