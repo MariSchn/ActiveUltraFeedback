8d20710dad636ceaab12d508f841888864f01202
mwertich <martin.wertich@gmail.com>
2025-08-05 10:48:12 +0200

    bugfix acquisition function

--- activeuf/acquisition_function/__init__.py
+++ activeuf/acquisition_function/__init__.py
@@ -4,6 +4,9 @@ from activeuf.acquisition_function.random import RandomAcquisitionFunction
 from activeuf.acquisition_function.maxminlcb import MaxMinLCB
 from activeuf.acquisition_function.infogain import InfoGain
 from activeuf.acquisition_function.ultrafeedback import UltraFeedback
+from activeuf.acquisition_function.ids import InformationDirectedSampling
+from activeuf.acquisition_function.rucb import RelativeUpperConfidenceBound
+
 
 __all__ = [
     "DoubleThompsonSampling",
