5bba0f3684bb848626fe9f9de2b3452a33a6f721
davmels <davmelous@gmail.com>
2025-08-14 14:40:30 +0200

    Removing old experimentation files, including only the latest annotation scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --account=a-infra01-1
-#SBATCH --partition=debug
-#SBATCH --time=00:30:00
-#SBATCH --container-writable
-#SBATCH --output=run_%j.out
-
-srun --environment=activeuf python -u run.py --part 0
