18f9c23c9202d8935fa3c7c8e277d5e5de44a397
Jessica Lam <jehong@ethz.ch>
2025-08-27 11:47:50 +0200

    ensure loading of local env vars always

--- activeuf/utils.py
+++ activeuf/utils.py
@@ -53,13 +53,12 @@ def get_logger(name: str, logs_path: str = "app.log") -> logging.Logger:
 def setup(login_to_hf: bool = False, login_to_wandb: bool = False) -> None:
     # load env variables
     load_dotenv(PUBLIC_ENV_PATH)
+    load_dotenv(LOCAL_ENV_PATH)
 
     if login_to_hf:
-        load_dotenv(LOCAL_ENV_PATH)
         huggingface_hub.login(os.getenv("HF_TOKEN"))
 
     if login_to_wandb:
-        load_dotenv(LOCAL_ENV_PATH)
         wandb.login(key=os.getenv("WANDB_TOKEN"))
 
 
