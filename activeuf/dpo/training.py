e589a917047f9cd2132e89ad8d94c48318bd959a
davmels <davmelous@gmail.com>
2025-09-25 00:56:57 +0200

    adapting the trainer script for multiple parallel job runs. Using my obtained hyperparameters that worked the best so far. modified the multi node to pass different hyperparameters

--- activeuf/dpo/training.py
+++ activeuf/dpo/training.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import yaml
 
 # from trl import DPOConfig
@@ -7,6 +8,7 @@ from trl.data_utils import apply_chat_template, extract_prompt
 from peft import LoraConfig, get_peft_model
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_from_disk, load_dataset
+from pprint import pprint
 
 from accelerate import Accelerator
 
@@ -15,12 +17,27 @@ from activeuf.utils import *
 from activeuf.dpo.trainer import NormedDPOConfig, NormedDPOTrainer
 
 
+"""
+run command example:
+accelerate launch \
+    --config_file=$SCRATCH/ActiveUltraFeedback/activeuf/reward_model/multi_gpu.yaml -m activeuf.dpo.training \
+    --config_path $SCRATCH/ActiveUltraFeedback/activeuf/dpo/training.yaml \
+    --slurm_job_id $SLURM_JOB_ID \
+    --dataset_path allenai/ultrafeedback_binarized_cleaned \
+    --beta 0.1
+"""
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config_path", type=str,
-                        help="Path to the YAML training config.")
-    parser.add_argument("--slurm_job_id", type=str,
-                        help="SLURM Job ID associated with this run")
+    parser.add_argument(
+        "--config_path", type=str, help="Path to the YAML training config."
+    )
+    parser.add_argument(
+        "--slurm_job_id", type=str, help="SLURM Job ID associated with this run"
+    )
+    parser.add_argument("--dataset_path", type=str, help="Path to the training dataset")
+    parser.add_argument("--beta", type=float, help="DPO beta parameter", default=0.1)
     return parser.parse_args()
 
 
@@ -34,7 +51,8 @@ def process_dataset(dataset):
         else:
             # TODO: More general way of handling dataset splits (They should come as arguments, for example).
             raise Exception(
-                "Unknown dataset format. Expected 'train' or 'train_prefs' split.")
+                "Unknown dataset format. Expected 'train' or 'train_prefs' split."
+            )
     else:
         train_dataset = dataset
 
@@ -42,6 +60,9 @@ def process_dataset(dataset):
 
 
 if __name__ == "__main__":
+    accelerator = Accelerator()
+    torch.cuda.empty_cache()
+    torch.cuda.reset_peak_memory_stats()
     # load env vars, args, configs
     setup()
     args = parse_args()
@@ -52,18 +73,30 @@ if __name__ == "__main__":
     lora_config = config.get("lora", {})
     training_config = config.get("training", {})
 
+    def sanitize_name(name):
+        # Replace / and . with _
+        return re.sub(r"[/.]", "_", name)
+
+    dataset_base = os.path.basename(args.dataset_path.rstrip("/"))
+    dataset_base = sanitize_name(dataset_base)
+
     # prepare output dir based on SLURM job id and run name
-    run_name = f"{args.slurm_job_id}-{config['base_run_name']}"
+    run_name = (
+        f"{args.slurm_job_id}-{dataset_base}-beta{str(args.beta).replace('.', '')}"
+    )
     output_dir = os.path.join(config["base_output_dir"], run_name)
+    if accelerator.is_main_process:
+        print(f"Output dir: {output_dir}")
+        print(f"Run name: {run_name}")
+        print(f"Dataset: {args.dataset_path}")
+        print("==== Training Arguments ====")
+        print(args)
+        print("==== YAML Configuration ====")
+        pprint(config)
 
     # set seed for reproducibility
     if isinstance(config.get("seed"), int):
         set_seed(config.get("seed"))
-
-    # prepare accelerator and torch stats
-    accelerator = Accelerator()
-    torch.cuda.empty_cache()
-    torch.cuda.reset_peak_memory_stats()
 
     # send config file to wandb
     if accelerator.is_main_process:
@@ -72,9 +105,15 @@ if __name__ == "__main__":
         artifact = wandb.Artifact(run_name, type="config")
         artifact.add_file(args.config_path)
         wandb.log_artifact(artifact)
-    
+
     # let all ranks wait here so that W&B is ready before training starts
     accelerator.wait_for_everyone()
+
+    # Export config file for reproducibility
+    out_path = os.path.join(output_dir, os.path.basename(args.config_path))
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    with open(out_path, "w") as f_out:
+        yaml.dump(config, f_out, default_flow_style=False)
 
     if config.get("torch_dtype", "bfloat16") == "bfloat16":
         torch_dtype = torch.bfloat16
@@ -82,8 +121,8 @@ if __name__ == "__main__":
         torch_dtype = torch.float32
 
     # load dataset, remove problematic columns
-    print(f"Loading dataset from {config["train_path"]}...\n")
-    dataset_path = config["train_path"]
+    print(f"Loading dataset from {args.dataset_path}...\n")
+    dataset_path = args.dataset_path
     # Load dataset
     try:
         dataset = load_dataset(dataset_path)
@@ -113,33 +152,41 @@ if __name__ == "__main__":
     # remove samples where prompt+chosen or prompt+rejected exceeds max length
     if training_config["max_length"]:
         temp = dataset.map(extract_prompt)
-        temp = temp.map(apply_chat_template, fn_kwargs={
-                        "tokenizer": tokenizer})
-        temp = temp.map(lambda _: NormedDPOTrainer.tokenize_row(
-            _,
-            processing_class=tokenizer,
-            max_prompt_length=None,
-            max_completion_length=None,
-            add_special_tokens=True,
-        ))
+        temp = temp.map(apply_chat_template, fn_kwargs={"tokenizer": tokenizer})
+        temp = temp.map(
+            lambda _: NormedDPOTrainer.tokenize_row(
+                _,
+                processing_class=tokenizer,
+                max_prompt_length=None,
+                max_completion_length=None,
+                add_special_tokens=True,
+            )
+        )
 
         old_n = len(dataset)
         print(f"Original number of samples: {old_n}")
+
         def check_if_short(x: dict) -> dict[str, bool]:
             return {
-                "is_short": len(x["prompt_input_ids"]) + len(x["chosen_input_ids"]) <= training_config["max_length"] or \
-                len(x["prompt_input_ids"]) + len(x["rejected_input_ids"]) <= training_config["max_length"]
+                "is_short": len(x["prompt_input_ids"]) + len(x["chosen_input_ids"])
+                <= training_config["max_length"]
+                or len(x["prompt_input_ids"]) + len(x["rejected_input_ids"])
+                <= training_config["max_length"]
             }
+
         temp = temp.map(check_if_short)
         idxs = [i for i, _ in enumerate(temp["is_short"]) if _]
         dataset = dataset.select(idxs)
-        print(f"Number of samples removed due to length constraints: {old_n - len(dataset)}")
+        print(
+            f"Number of samples removed due to length constraints: {old_n - len(dataset)}"
+        )
 
     dataset = dataset.select_columns(["chosen", "rejected"])
 
     # create lora version of model
     model = AutoModelForCausalLM.from_pretrained(
-        model_path, trust_remote_code=True, torch_dtype=torch_dtype)
+        model_path, trust_remote_code=True, torch_dtype=torch_dtype
+    )
     peft_config = LoraConfig(**lora_config)
     try:
         model = get_peft_model(model, peft_config)
@@ -155,12 +202,17 @@ if __name__ == "__main__":
             peft_config.target_modules = target_modules
         else:
             print(
-                f"Target module patterns for {model_path} must be implemented manually")
+                f"Target module patterns for {model_path} must be implemented manually"
+            )
             raise
 
     # create DPO trainer
     trainer_config = NormedDPOConfig(
-        run_name=run_name, output_dir=output_dir, dataset_num_proc=accelerator.num_processes, **training_config)
+        run_name=run_name,
+        output_dir=output_dir,
+        dataset_num_proc=accelerator.num_processes,
+        **training_config,
+    )
     trainer = NormedDPOTrainer(
         model=model,
         args=trainer_config,
@@ -177,10 +229,6 @@ if __name__ == "__main__":
         tokenizer.save_pretrained(output_dir)
         print(f"Model and tokenizer saved to {output_dir}")
 
-    # Export config file for reproducibility
-    out_path = os.path.join(output_dir, os.path.basename(args.config_path))
-    with open(out_path, "w") as f_out:
-        yaml.dump(config, f_out, default_flow_style=False)
 
 if accelerator.is_main_process and wandb.run is not None:
     wandb.finish()
