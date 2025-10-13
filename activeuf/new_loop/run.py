509d20b4a443a346e887874565b8eaa6cc7ffe00
Jessica Lam <jehong@ethz.ch>
2025-10-13 19:48:46 +0200

    refactored up to wandb KPI logging, replay buffer extending. trying to make trainer work without needing input_ids

--- activeuf/new_loop/run.py
+++ activeuf/new_loop/run.py
@@ -1,125 +1,24 @@
 from accelerate import Accelerator
 from accelerate.utils import gather_object
-from collections import deque
+from collections import defaultdict, deque
 from datasets import Dataset, load_from_disk
 import json
 import os
+import random
 import regex as re
 import time
 import torch
 from torch.utils.data import DataLoader
+import wandb
 
 from activeuf.acquisition_function import init_acquisition_function
 from activeuf.new_loop.arguments import get_args
-from activeuf.new_loop.utils import init_reward_model_tokenizer_trainer, custom_collate, custom_decollate
+from activeuf.new_loop.utils import *
 from activeuf.oracle.oracles import init_oracle
 from activeuf.utils import get_logger, set_seed
 
 # RUN
 # python -m activeuf.new_loop.run --config_path activeuf/new_loop/run.yaml
-
-def compute_rewards(samples, reward_model = None) -> torch.tensor:
-    n_samples = len(samples)
-    n_completions_per_sample = len(samples[0]["completions"])
- 
-    # TODO: make acquisition function type an attr of the acquisition function
-    if reward_model is None:
-        return torch.zeros(
-            (n_samples, n_completions_per_sample, 3),
-            dtype=torch.float32,
-        )
-
-    def get_features_yielder():
-        for sample in samples:
-            for completion in sample["completions"]:
-                yield torch.tensor(completion["features"])
-
-    features_yielder = get_features_yielder()
-    rewards_batch = []
-    while True:
-        features_mbatch = []
-        for _ in range(args.compute_reward_batch_size):
-            try:
-                features_mbatch.append(next(features_yielder))
-            except StopIteration:
-                break
-        if not features_mbatch:
-            break
-        features_mbatch = torch.stack(features_mbatch).to(reward_model.device)
-
-        with torch.no_grad():
-            output = reward_model(features=features_mbatch)
-
-        rewards_batch.extend(output["rewards"].cpu())
-
-    torch.cuda.empty_cache()
-    rewards_batch = torch.stack(rewards_batch).view(n_samples, -1, 3)
-
-    return rewards_batch
-
-def get_acquired(samples, acquired_idxs, tokenizer = None):
-    acquired = []
-    for sample, (a, b) in zip(samples, acquired_idxs):
-        completions = sample["completions"]
-
-        acquired.append({
-            "prompt_id": sample["prompt_id"],
-            "prompt": sample["prompt"],
-            "source": sample["source"],
-            "row_id": sample["row_id"],
-
-            "response_text_1": completions[a]["response_text"],
-            "1_model": completions[a]["model"],
-            "1_score": completions[a]["overall_score"],
-
-            "response_text_2": completions[b]["response_text"],
-            "2_model": completions[b]["model"],
-            "2_score": completions[b]["overall_score"],
-        })
-
-    if tokenizer is None:
-        return acquired
-
-    messages = []
-    for sample, (a, b) in zip(samples, acquired_idxs):
-        for idx in [a, b]:
-            messages.append([
-                {"role": "user", "content": sample["prompt"]},
-                {
-                    "role": "assistant", 
-                    "content": sample["completions"][idx]["response_text"],
-                },
-            ])
-    inputs = tokenizer(
-        tokenizer.apply_chat_template(messages, tokenize=False),
-        padding="longest",
-        max_length=args.max_length,
-        truncation=True,
-        return_tensors="pt",
-    )
-    dim = inputs["input_ids"].shape[-1]
-    input_ids = inputs["input_ids"].view(-1, 2, dim)
-    attention_mask = inputs["attention_mask"].view(-1, 2, dim)
-
-    for i, (a,b) in enumerate(acquired_idxs):
-        acquired[i].update({
-            "features_1": samples[i]["completions"][a]["features"],
-            "input_ids_1": input_ids[i][0].tolist(),
-            "attention_mask_1": attention_mask[i][0].tolist(),
-
-            "features_2": samples[i]["completions"][b]["features"],
-            "input_ids_2": input_ids[i][1].tolist(),
-            "attention_mask_2": attention_mask[i][1].tolist(),
-        })
-    return acquired
-
-def restructure_sample_for_rewardtrainer(x: dict) -> dict:
-    for key in ["chosen", "rejected"]:
-        x[key] = [
-            {"content": x["prompt"], "role": "user"},
-            {"content": x[key], "role": "assistant"},
-        ]
-    return x
 
 if __name__ == "__main__":
     args = get_args()
@@ -155,13 +54,11 @@ if __name__ == "__main__":
 
     # TODO but low priority: use a init function to allow for different reward models
     # TODO bypass pipeline and construct directly?
-    # TODO: ask whether wandb callback for kpi logging is still necessary, given how it's set to False by default
     logger.info(f"Preparing reward model, tokenizer, and trainer ({args.reward_model})")
-    reward_trainer_config = args.reward_trainer_config[args.reward_model]
-    reward_model, tokenizer, trainer = init_reward_model_tokenizer_trainer(
-        args,
-        batch_size=reward_trainer_config["effective_batch_size"] // n_processes,
+    reward_model, tokenizer, trainer, trainsize = init_reward_model_tokenizer_trainer_trainsize(
+        args, n_processes
     )
+    initial_regularization = float(trainer.args.regularization_towards_initial_weights)
 
     # wait for everyone to finish loading the models
     accelerator.wait_for_everyone()
@@ -169,21 +66,20 @@ if __name__ == "__main__":
     logger.info(f"Loading prompts from {args.inputs_path}")
     dataset = load_from_disk(args.inputs_path)
     if args.debug:
-        dataset = dataset.select(range(100))
+        dataset = dataset.select(range(200))
     if "row_id" not in dataset.column_names:
-        dataset = dataset.add_column("row_id", list(range(len(dataset))))
-    logger.info(f"# Prompts: {len(dataset)}")
-    
-    # TODO: find a nicer place for this someday. for now assume that everything has precomputed features
+        dataset = dataset.add_column("row_id", list(range(len(dataset))))    
     if "features" not in dataset[0]["completions"][0]:
         logger.info("Please precompute features before running this script")
         exit(1)
-
-    # TODO: figure out how to continue from a previous run of the loop
-    # TODO: generalise replay buffer part because not all reward models will need it
-    output = []
-    replay_buffer = deque(maxlen=reward_trainer_config["replay_buffer_size"])
     dataset = dataset.shuffle(seed=args.seed)
+    logger.info(f"# Prompts: {len(dataset)}")
+
+    # prepare output container and trainer replay buffer
+    output = []
+    replay_buffer = deque(maxlen=args.replay_buffer_size)
+
+    logger.info(f"Starting dataset generation loop")
     outer_dataloader = DataLoader(
         dataset, 
         batch_size=args.outer_loop_batch_size,
@@ -191,8 +87,6 @@ if __name__ == "__main__":
         shuffle=False,
         drop_last=False,
     )
-
-    logger.info(f"Starting dataset generation loop")
     for outer_batch_idx, outer_batch in enumerate(outer_dataloader):
         if accelerator.is_main_process:
             logger.info(f"Step {outer_batch_idx+1} / {len(outer_dataloader)}")
@@ -204,18 +98,18 @@ if __name__ == "__main__":
             shuffle=False,
             drop_last=False,
         )
+        logger.info(f"- # Minibatches: {len(dataloader)}")
         dataloader = accelerator.prepare(dataloader)
 
         annotated_batch = []
+        kpi_samplewise_batch = []
+        kpi_batch = defaultdict(list)
         for collated_minibatch in dataloader:
             samples_local = custom_decollate(collated_minibatch)
 
             start = time.time()
-            if args.acquisition_function == "random":
-                rewards_local = compute_rewards(samples_local)
-            else:
-                reward_model.eval()
-                rewards_local = compute_rewards(samples_local, reward_model)
+            rewards_local = compute_rewards(
+                samples_local, reward_model, args.compute_reward_batch_size)
             logger.info(f"- Reward computation took {time.time() - start:.2f}s")
 
             start = time.time()
@@ -225,38 +119,88 @@ if __name__ == "__main__":
             logger.info(f"- Acquisition function took {time.time() - start:.2f}s")
 
             start = time.time()
-            if args.acquisition_function == "random":
-                acquired_local = get_acquired(samples_local, acquired_idxs_local)
-            else:
-                acquired_local = get_acquired(
-                    samples_local, acquired_idxs_local, tokenizer)
+            acquired_local = get_acquired(samples_local, acquired_idxs_local)
             logger.info(f"- Preparing acquired batch took {time.time() - start:.2f}s")
 
             start = time.time()
-            annotated_local = oracle(acquired_local)
+            annotated_local = [restructure_sample(x) for x in oracle(acquired_local)]
             logger.info(f"- Oracle annotation took {time.time() - start:.2f}s")
 
-            # TODO: KPI reporting
+            start = time.time()
+            kpi_samplewise_local, kpi_minibatchwise_local = compute_kpi(
+                rewards_local, acquired_idxs_local,
+            )
+            logger.info(f"- KPI computation took {time.time() - start:.2f}s")
 
             start = time.time()
             accelerator.wait_for_everyone()
-            annotated_minibatch = gather_object(annotated_local)
-            annotated_batch += annotated_minibatch
+            annotated_batch += gather_object(annotated_local)
+            kpi_samplewise_batch += gather_object(kpi_samplewise_local)
+            for key, val in gather_object(kpi_minibatchwise_local).items():
+                kpi_batch[key].append(val)
+
             logger.info(f"- Gathering data from processes took {time.time() - start:.2f}s")
 
-        # restructure data for reward trainer
-        annotated_batch = [
-            restructure_sample_for_rewardtrainer(x) for x in annotated_batch
-        ]
-
         if accelerator.is_main_process:
-            output += [{
-                k: v for k, v in x.items()
-                if not re.search(r"features|input_ids|attention_mask", k)
-            } for x in annotated_batch]
-            print(f"- Acquired {len(annotated_batch)} samples, total {len(output)}")
+            # remove features from being saved to disk
+            output += [
+                {key: val for key, val in x.items() if "features" not in key} 
+                for x in annotated_batch
+            ]
+            logger.info(f"Current output dataset size: {len(output)}")
 
-    
+            if outer_batch_idx % args.save_every_n_outer_batches == 0:
+                logger.info(f"Writing output dataset to {args.output_path}")
+                Dataset.from_list(output).save_to_disk(args.output_path)
+
+            if args.report_to == "wandb":
+                logger.info("Reporting KPIs to WandB")
+                wandb.init(
+                    project=args.wandb_project,
+                    id=args.run_id,
+                    config=vars(args),
+                    resume=(outer_batch_idx > 0),
+                    allow_val_change=True,
+                )
+                for idx, kpi in enumerate(kpi_samplewise_batch):
+                    if idx == len(kpi_samplewise_batch) - 1:
+                        # log batch-level metrics alongside last sample
+                        for key, val in kpi_batch.items():
+                            kpi[key] = sum(val) / len(val)
+                    wandb.log(kpi)
+                wandb.finish()
+
+        if trainer is None:
+            logger.info("Skipping reward model training")
+            continue
+        
+        logger.info(f"Adding fresh batch to replay buffer, then subsampling {trainsize} for training")
+        # TODO: discuss -- shouldn't input_ids be unnecessary?
+        replay_buffer.extend([{
+            "features_chosen": x["features_chosen"],
+            "features_rejected": x["features_rejected"],
+        } for x in annotated_batch])
+        trainer.train_dataset = Dataset.from_list(random.sample(
+            replay_buffer, min(len(replay_buffer), trainsize),
+        ))
+
+        logger.info(f"Updating regularization")
+        trainer.args.regularization_towards_initial_weights = update_regularization(
+            initial_regularization, 
+            outer_batch_idx, 
+            args.outer_loop_batch_size,
+            args.reward_trainer_config[args.reward_model],
+        )
+
+        reward_model.train()
+        start = time.time()
+        trainer.train()
+        logger.info(f"Reward model training took {time.time() - start:.2f}s")
+        
+        torch.cuda.empty_cache()
+        
+        # TODO: log to wandb
+
     if accelerator.is_main_process and len(output) > 0:
         logger.info(f"Saving generated dataset to {args.output_path}")
         Dataset.from_list(output).save_to_disk(args.output_path)
